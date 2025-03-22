4c65b9a580f580a9f6ed733a8e49392e80782ff0
horsjaco117 <jacob_horsley@live.com>
2025-03-22 17:10:37 -0600

    All the tool tips and UI cleaning is done

--- RolloftheDice/Form1.Designer.vb
+++ RolloftheDice/Form1.Designer.vb
@@ -22,62 +22,100 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.RollButton = New System.Windows.Forms.Button()
         Me.ClearButton = New System.Windows.Forms.Button()
         Me.ExitButton = New System.Windows.Forms.Button()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'RollButton
         '
         Me.RollButton.AutoSize = True
-        Me.RollButton.Location = New System.Drawing.Point(125, 366)
+        Me.RollButton.Location = New System.Drawing.Point(26, 347)
         Me.RollButton.Name = "RollButton"
-        Me.RollButton.Size = New System.Drawing.Size(75, 30)
+        Me.RollButton.Size = New System.Drawing.Size(117, 73)
         Me.RollButton.TabIndex = 0
         Me.RollButton.Text = "Roll"
+        Me.ToolTip1.SetToolTip(Me.RollButton, "Rolls all the dice a large amount of times")
         Me.RollButton.UseVisualStyleBackColor = True
         '
         'ClearButton
         '
         Me.ClearButton.AutoSize = True
-        Me.ClearButton.Location = New System.Drawing.Point(293, 381)
+        Me.ClearButton.Location = New System.Drawing.Point(268, 347)
         Me.ClearButton.Name = "ClearButton"
-        Me.ClearButton.Size = New System.Drawing.Size(75, 30)
+        Me.ClearButton.Size = New System.Drawing.Size(125, 73)
         Me.ClearButton.TabIndex = 1
         Me.ClearButton.Text = "Clear"
+        Me.ToolTip1.SetToolTip(Me.ClearButton, "Clears all the data along with the text")
         Me.ClearButton.UseVisualStyleBackColor = True
         '
         'ExitButton
         '
         Me.ExitButton.AutoSize = True
-        Me.ExitButton.Location = New System.Drawing.Point(464, 394)
+        Me.ExitButton.Location = New System.Drawing.Point(521, 347)
         Me.ExitButton.Name = "ExitButton"
-        Me.ExitButton.Size = New System.Drawing.Size(75, 30)
+        Me.ExitButton.Size = New System.Drawing.Size(130, 73)
         Me.ExitButton.TabIndex = 2
         Me.ExitButton.Text = "Exit"
+        Me.ToolTip1.SetToolTip(Me.ExitButton, "Exits the program")
         Me.ExitButton.UseVisualStyleBackColor = True
         '
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
         Me.ListBox1.ItemHeight = 20
-        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Location = New System.Drawing.Point(26, 48)
         Me.ListBox1.Name = "ListBox1"
         Me.ListBox1.Size = New System.Drawing.Size(625, 284)
         Me.ListBox1.TabIndex = 3
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(682, 33)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(73, 29)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        Me.MenuToolStripMenuItem.ToolTipText = "Opens a special menu"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(65, 29)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        Me.HelpToolStripMenuItem.ToolTipText = "Helps the user utilize program"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(682, 450)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.ExitButton)
         Me.Controls.Add(Me.ClearButton)
         Me.Controls.Add(Me.RollButton)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -87,4 +125,8 @@ Partial Class Form1
     Friend WithEvents ClearButton As Button
     Friend WithEvents ExitButton As Button
     Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolTip1 As ToolTip
 End Class
--- RolloftheDice/Form1.vb
+++ RolloftheDice/Form1.vb
@@ -1,23 +1,21 @@
 ﻿Imports System.Windows.Markup
 
 Public Class Form1
-    Dim beanCounter(12) As Integer
+    Dim diceCounter(12) As Integer
     Sub diceRoll()
-        'Dim beanCounter(12) As Integer
-        TestRandomness(beanCounter) 'This runs the random number generator
-        'DisplayBoard(beanCounter) 'Things are organized into a board
+
+        RandomGen(diceCounter) 'This runs the random number generator
+
     End Sub
 
-    Sub TestRandomness(ByRef beanCounter() As Integer) 'Random generator
+    Sub RandomGen(ByRef diceCounter() As Integer) 'Random generator
 
         For i = 1 To 1000
 
-            beanCounter(randomNumberBetween(1, 13)) += 1 'the return is the pointer for the array
-
+            diceCounter(randomNumberBetween(1, 13)) += 1 'the return is the pointer for the array
         Next
 
     End Sub
-
 
     Function randomNumberBetween(max As Integer, min As Integer) As Integer
         Dim temp As Single 'The single type helps work with the randomize stuff
@@ -27,39 +25,51 @@ Public Class Form1
         Return CInt(Math.Floor(temp)) 'min isn't included
     End Function
 
-
-    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
-
-    End Sub
-
     Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
         diceRoll()
 
+        Dim header As String = ""
+        Dim width As Integer = 6
+
         ListBox1.Items.Clear()
         ListBox1.Items.Add("Roll of The Dice")
-        ListBox1.Items.Add("- - - - - - - - - - - - - - - - - - - - -")
+        ListBox1.Items.Add("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
 
         ' Horizontal Header settings
-        Dim header As String = ""
         For i = 2 To 12
-            header &= i.ToString().PadRight(6) & "| " 'the padding helps align the top row
+            header &= i.ToString().PadLeft(width) & " |" 'the padding helps align the top row
         Next
         ListBox1.Items.Add(header)
+
+        ListBox1.Items.Add("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
 
         ' This adds in the values for both rows
         Dim values As String = ""
         For i = 2 To 12
-            values &= beanCounter(i).ToString().PadRight(5) & "| " 'Align the bottom row
+            values &= diceCounter(i).ToString().PadLeft(width) & "| " 'Align the bottom row
         Next
         ListBox1.Items.Add(values)
+
+        ListBox1.Items.Add("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+
     End Sub
 
-
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+
         ListBox1.Items.Clear()
+        Array.Clear(diceCounter, 0, diceCounter.Length)
+
     End Sub
 
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
+    End Sub
+
+    Private Sub MenuToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MenuToolStripMenuItem.Click
+        MsgBox("You found an Easter Egg!")
+    End Sub
+
+    Private Sub HelpToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HelpToolStripMenuItem.Click
+        MsgBox("Hit the buttons and see which numbers appear")
     End Sub
 End Class
