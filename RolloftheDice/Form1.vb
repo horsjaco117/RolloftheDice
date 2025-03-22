9c98ba1b424eeb9a45e9da3ddd3763db47f18b7f
horsjaco117 <jacob_horsley@live.com>
2025-03-22 16:18:06 -0600

    Horizontal formatting finally configured.

--- RolloftheDice/Form1.vb
+++ RolloftheDice/Form1.vb
@@ -1,5 +1,7 @@
-﻿Public Class Form1
- Dim beanCounter(12) As Integer
+﻿Imports System.Windows.Markup
+
+Public Class Form1
+    Dim beanCounter(12) As Integer
     Sub diceRoll()
         'Dim beanCounter(12) As Integer
         TestRandomness(beanCounter) 'This runs the random number generator
@@ -18,12 +20,12 @@
 
 
     Function randomNumberBetween(max As Integer, min As Integer) As Integer
-            Dim temp As Single 'The single type helps work with the randomize stuff
-            Randomize()
-            temp = Rnd()
-            temp = temp * (max - min + 1) + min
-            Return CInt(Math.Floor(temp)) 'min isn't included
-        End Function
+        Dim temp As Single 'The single type helps work with the randomize stuff
+        Randomize()
+        temp = Rnd()
+        temp = temp * (max - min + 1) + min
+        Return CInt(Math.Floor(temp)) 'min isn't included
+    End Function
 
 
     Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
@@ -33,17 +35,25 @@
     Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
         diceRoll()
 
-        ListBox1.Items.Add("Test Line")
-        ' Display results in ListBox
-        ListBox1.Items.Add("Dice Roll Results:")
-        ListBox1.Items.Add("Roll  | Count")
-        ListBox1.Items.Add("-----------------")
+        ListBox1.Items.Clear()
+        ListBox1.Items.Add("Roll of The Dice")
+        ListBox1.Items.Add("- - - - - - - - - - - - - - - - - - - - -")
 
+        ' Horizontal Header settings
+        Dim header As String = ""
         For i = 2 To 12
-            ListBox1.Items.Add(i.ToString().PadRight(5) & " | " & beanCounter(i).ToString())
+            header &= i.ToString().PadRight(6) & "| " 'the padding helps align the top row
         Next
+        ListBox1.Items.Add(header)
 
+        ' This adds in the values for both rows
+        Dim values As String = ""
+        For i = 2 To 12
+            values &= beanCounter(i).ToString().PadRight(5) & "| " 'Align the bottom row
+        Next
+        ListBox1.Items.Add(values)
     End Sub
+
 
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         ListBox1.Items.Clear()
